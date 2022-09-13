d19e108eb974cf11406e157925a379a6e1b815b0
Kwon <anstjdrnjs3@gmail.com>
2022-09-13 17:37:14 +0900

    using login user anotation

--- src/main/java/com/jojoldu/book/springboot/controller/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.controller;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.controller.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.service.posts.PostsService;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
